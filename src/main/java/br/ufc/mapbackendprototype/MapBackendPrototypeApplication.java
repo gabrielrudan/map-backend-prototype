0ad112d8fc8db8319cd780ce4b8a956fe6f804f0
gabrielrudan <gabrielrudam92@gmail.com>
2026-08-26 23:00:49 -0300

    chore: inicializar projeto backend com Spring Boot #83

---
+++ src/main/java/br/ufc/mapbackendprototype/MapBackendPrototypeApplication.java
@@ -0,0 +1,13 @@
+package br.ufc.mapbackendprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapBackendPrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapBackendPrototypeApplication.class, args);
+    }
+
+}
